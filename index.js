86332b3781df18b51b2c32b20b347ee46018458c
Gabriel Trompiz <gab.tc@icloud.com>
2018-10-27 20:15:02 -0400

    added bgs with rotating animation, auto-contrast on menu

--- index.js
+++ index.js
@@ -3,6 +3,31 @@ var single = document.getElementById('single');
 var multi = document.getElementById('multi');
 var	settings = document.getElementById('settings');
 var quit = document.getElementById('quit');
+var game = document.getElementById('game');
+
+var counter = 1;
+
+setInterval(function() {
+	counter++;
+	console.log(counter);
+	game.style.backgroundImage = "url(assets/" + counter + ".png)";
+	if (counter == 2 || counter == 3 || counter == 4 || counter == 6 || counter == 7 || 
+		counter == 8 || counter == 9 || counter == 11) {
+		logo.style.color = "white";
+		single.style.color = "white";
+		multi.style.color = "white";
+		settings.style.color = "white";
+		quit.style.color = "white";
+	}
+	else {
+		logo.style.color = "black";
+		single.style.color = "black";
+		multi.style.color = "black";
+		settings.style.color = "black";
+		quit.style.color = "black";
+	}
+	if (counter == 11) { counter = 0; }
+}, 5000)
 
 function hide(o) {
 	o.className = "hidden";
@@ -21,6 +46,8 @@ single.onclick = (e) => {
 	hide(settings);
 	hide(logo);
 	hide(single);
+	console.log(game);
+	game.className = "fight";
 }
 
 quit.onclick = (e) => {
