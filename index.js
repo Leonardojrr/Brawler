4e0748666d9becdd62aa0caa7ce36ffa570fe23d
Gabriel Trompiz <gab.tc@icloud.com>
2018-10-30 23:52:44 -0400

    p1 able to punch

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ var game = document.getElementById('game');
 var menu = document.getElementById('menu');
 var scene = document.getElementById('scene');
 scene.style.visibility = 'hidden';
-var ryu = new Array();
-
+var ctx = scene.getContext('2d');
+ctx.canvas.width = window.innerWidth;
+ctx.canvas.height = window.innerHeight;
+var p1 = new Array();
+var p2 = new Array();
+var intervalP1, intervalP2;
 var counter = 1;
+var i = 0;
+var j = 0;
+var isP1stance = true;
 
 var menuRotate = setInterval(function() {
 	counter++;
@@ -33,28 +40,66 @@ var menuRotate = setInterval(function() {
 	if (counter == 11) { counter = 0; }
 }, 2000)
 
+function animateP2(p2x, p2y, src, frames) {
+	j = 0;
+	clearInterval(intervalP2);
+	intervalP2 = setInterval( function() {
+		p2[j] = new Image();
+		p2[j].src = src + j + '.png';
+		ctx.clearRect(p2x, p2y, (p2x + p2[j].width * 2.5), (p2y + p2[j].height * 2.5));
+		ctx.drawImage(p2[j], p2x, p2y, parseInt(p2[j].width * 2.5), parseInt(p2[j].height * 2.5));
+		if (j == frames - 1) {
+			j = 0;
+		}
+		else {
+			j++;
+		}
+	}, 55);
+}
+
+function animateP1(p1x, p1y, src, frames) {
+		i = 0;
+		clearInterval(intervalP1);
+		intervalP1 = setInterval( function() {
+			p1[i] = new Image();
+			p1[i].src = src + i + '.png';
+			ctx.clearRect(p1x, p1y, (p1x + p1[i].width * 2.5), (p1y + p1[i].height * 2.5));
+			ctx.drawImage(p1[i], p1x, p1y, parseInt(p1[i].width * 2.5), parseInt(p1[i].height * 2.5));
+			if (i == frames - 1 && isP1stance) {
+				i = 0;
+			}
+			else if (!isP1stance && ++i === frames) {
+				console.log('woo');
+				isP1stance = true;
+				clearInterval(intervalP1);
+				animateP1(p1x, p1y, 'D:\\Dev\\wip\\brawler\\Brawler\\assets\\ryu\\stance-left-', 10);
+			}
+			else {
+				i++;
+			}
+		}, 55);	
+}
+
 single.onclick = (e) => {
 	for (var i = menu.childNodes.length - 1; i >= 0; i--) {
 		menu.childNodes[i].className = "hidden";
 	}
 	clearInterval(menuRotate);
-	var ctx = scene.getContext('2d');
-	ctx.canvas.width = window.innerWidth;
-	ctx.canvas.height = window.innerHeight;
 	scene.style.visibility = "visible";
-	var i = 0;
-	setInterval( function() {
-		ryu[i] = new Image();
-		ryu[i].src = 'D:\\Dev\\wip\\brawler\\Brawler\\assets\\ryu\\stance-right-' + i + '.png'
-		ctx.clearRect(0, 0, scene.width, scene.height);
-		ctx.drawImage(ryu[i], 1300, 400, parseInt(ryu[i].width * 2.5), parseInt(ryu[i].height * 2.5));
-		if (i == 9) {
-			i = 0;
+	animateP1(70, 400, 'D:\\Dev\\wip\\brawler\\Brawler\\assets\\ryu\\stance-left-', 10);
+	animateP2(1300, 400, 'D:\\Dev\\wip\\brawler\\Brawler\\assets\\ryu\\stance-right-', 10);
+	var shouldHandle = true;
+	onkeydown = (e) => {
+		if (shouldHandle) {
+			var key = e.keyCode || e.which;
+			if (key == 69) {
+				isP1stance = false;
+				shouldHandle = false;
+				animateP1(70, 400, 'D:\\Dev\\wip\\brawler\\Brawler\\assets\\ryu\\punch-left-', 5);
+				setTimeout(function() { shouldHandle = true }, 270);
+			}
 		}
-		else {
-			i++;
-		}
-	}, 55);
+	}
 }
 
 quit.onclick = (e) => {
